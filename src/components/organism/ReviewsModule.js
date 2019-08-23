455bd87fc4068c0ff64de712159395f0063176b4
zloyleva <zloyleva.imt@gmail.com>
2019-08-23 20:32:23 +0300

    show reviews rate stars

--- src/components/organism/ReviewsModule.js
+++ src/components/organism/ReviewsModule.js
@@ -1,15 +1,67 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Col from "react-bootstrap/Col";
 
 import ReviewsList from "./ReviewsList";
 
-const ReviewsModule = ({reviews}) => {
-    return (
-        <Col xs={6}>
-            <h2>Reviews</h2>
-            <ReviewsList reviews={reviews}/>
-        </Col>
-    )
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+import {faStar} from "@fortawesome/free-solid-svg-icons";
+import {faStar as faRegStar} from "@fortawesome/free-regular-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+class ReviewsModule extends Component{
+
+    state = {
+        indexHoverStar:-1,
+    }
+
+    setIndexHoverStar = (index) => {
+        console.log("onMouseOver",index)
+        this.setState({
+            indexHoverStar:index
+        })
+    }
+
+    clearHoveredStars = () => {
+        this.setState({
+            indexHoverStar:-1
+        })
+    }
+
+    render() {
+        const {reviews} = this.props;
+        const {indexHoverStar} = this.state;
+        return (
+            <Col xs={6}>
+                <h2>Add new review</h2>
+
+                <Form>
+                    <Form.Group controlId="exampleForm.ControlInput1" onMouseLeave={this.clearHoveredStars}>
+                        {
+                            [...Array(5)].map((el,i) => {
+                                return (
+                                    <FontAwesomeIcon
+                                        icon={indexHoverStar>=i?faStar:faRegStar}
+                                        onClick={()=>{}}
+                                        onMouseOver={()=>{this.setIndexHoverStar(i)}}
+                                    />
+                                )
+                            })
+                        }
+                    </Form.Group>
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Control as="textarea" rows="3" placeholder="Type your reviews..." />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+                <h2 className="mt-5">Reviews</h2>
+                <ReviewsList reviews={reviews}/>
+            </Col>
+        )
+    }
 }
 
 export default ReviewsModule;
--- src/components/organism/StarsList.js
+++ src/components/organism/StarsList.js
@@ -4,7 +4,7 @@ import Star from "../moleculas/Star";
 const StarsList = ({item}) => (
     <div>
         {
-            [...Array(5)].map((el,i) => <Star index={i} item={item} />)
+            [...Array(5)].map((el,i) => <Star index={i} item={item} key={i}/>)
         }
     </div>
 )
