76c15b602cf21df772eb026114ad6a3970506770
zloyleva <zloyleva.imt@gmail.com>
2019-08-28 19:43:21 +0300

    add Registration query

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,7 +17,7 @@ class App extends Component {
       );
     }
     return (
-      <Registration/>
+      <Registration />
     )
   }
 }
--- src/components/organism/Registration.js
+++ src/components/organism/Registration.js
@@ -5,16 +5,41 @@ import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
+import axios from 'axios';
+
 class Registration extends Component{
 
     state = {
         username: '',
-        password: ''
+        password: '',
+        loginUrl: 'http://smktesting.herokuapp.com/api/register/'
     }
 
     submitRegistration = (e) => {
         e.preventDefault()
-        console.log('submitRegistration')
+        const { username, password, loginUrl } = this.state;
+
+        axios
+            .post(
+                loginUrl,
+                {
+                        username,
+                        password
+                    }
+                )
+            .then(res => {
+                console.log('submitRegistration',res.data)
+
+
+            })
+
+    }
+
+    onChangeInput = (e) => {
+        console.log(e.target.name)
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
 
     render(){
@@ -26,12 +51,12 @@ class Registration extends Component{
                         <Form onSubmit={this.submitRegistration}>
                             <Form.Group controlId="formBasicUserName">
                                 <Form.Label>Name</Form.Label>
-                                <Form.Control type="text" placeholder="Enter your name" name="username" onChange={() => console.log('aklsjdfhlaks')} />
+                                <Form.Control type="text" placeholder="Enter your name" name="username" onChange={(e) => this.onChangeInput(e)} />
                             </Form.Group>
 
                             <Form.Group controlId="formBasicPassword">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" name="password" placeholder="Enter your password" />
+                                <Form.Control type="password" name="password" placeholder="Enter your password" onChange={(e) => this.onChangeInput(e)} />
                             </Form.Group>
                             <Button variant="primary" type="submit">
                                 Register
